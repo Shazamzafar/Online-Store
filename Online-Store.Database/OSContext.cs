fd09ab0830e250dfc838a5468d77638dc0f02a16
shaza <shaza@SHAZAM-ZAFAR>
2020-02-15 18:53:06 -0500

    Added Product and Category Bussiness Entity and created database

---
+++ Online-Store.Database/OSContext.cs
@@ -0,0 +1,21 @@
+﻿using Online_Store.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Store.Database
+{
+  public  class OSContext : DbContext 
+    {
+        public OSContext() : base("Online-StoreConnection")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Online-Store.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Online_Store.Web.Startup))]
+namespace Online_Store.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
