d2bc8d5ea72d7102706e741cdd8e548df8f2dc1f
shaza <shaza@SHAZAM-ZAFAR>
2020-02-20 17:42:00 -0500

    Crud Ajax Complete, Theme Application success

--- Online-Store.Database/OSContext.cs
+++ Online-Store.Database/OSContext.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Online_Store.Database
 {
-  public  class OSContext : DbContext 
+  public  class OSContext : DbContext, IDisposable
     {
         public OSContext() : base("Online-StoreConnection")
         {
---
+++ Online-Store.Services/CategoriesServices.cs
@@ -0,0 +1,64 @@
+﻿using Online_Store.Database;
+using Online_Store.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Online_Store.Services
+{
+   public class CategoriesServices
+    {
+
+        public Category GetCategory(int ID)
+        {
+            using (var context = new OSContext())
+            {
+                return context.Categories.Find(ID);
+            }
+        }
+
+        public  List<Category> GetCategories()
+        {
+            using (var context = new OSContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+
+        public void SaveCategory(Category category)
+        {
+            using (var context = new OSContext())
+            {
+                context.Categories.Add(category);
+
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            using (var context = new OSContext())
+            {
+
+                context.Entry(category).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+
+        public void DeleteCategory(int ID)
+        {
+            using (var context = new OSContext())
+            {
+
+
+                var category = context.Categories.Find(ID);
+
+                context.Categories.Remove(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
